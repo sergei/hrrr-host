8693816849d5cd1dd753e86a12365e169c2992bc
Sergei <spodshivalov@scmarinetech.com>
2024-04-01 16:16:14 -0700

    Added multiple regions

--- grib/download_hrrr.py
+++ grib/download_hrrr.py
@@ -10,6 +10,18 @@ from botocore import UNSIGNED
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
+REGIONS = [
+    {'name': 'new-england',
+     'nw': {'lat': 42, 'lon': -72},
+     'se': {'lat': 41, 'lon': -70}},
+    {'name': 'san-francisco',
+     'nw': {'lat': 38, 'lon': -123.3},
+     'se': {'lat': 36.5, 'lon': -121.5}},
+    {'name': 'coastal-cup',
+     'nw': {'lat': 38, 'lon': -123.3},
+     'se': {'lat': 33, 'lon': -120.}},
+]
+
 if platform.system() == 'Darwin':
     WGRIB_BIN = os.path.expanduser('bin/osx/wgrib2')
 else:
@@ -38,7 +50,7 @@ def parse_grib_index(grib_index):
     return records
 
 
-def download_grib_slice(s3, grib_name, work_dir, height_filter, type_filter):
+def download_grib_time_slice(s3, grib_name, work_dir, height_filter, type_filter):
 
     # Download GRIB index file
     grib_idx_name = grib_name + '.idx'
@@ -63,16 +75,7 @@ def download_grib_slice(s3, grib_name, work_dir, height_filter, type_filter):
                     gf.write(res)
             print(f'{local_grib_name} created.')
 
-            t = local_grib_name.split('.')
-            small_local_grib_name = '.'.join(t[0:-1]) + '.small.' + t[-1]
-
-            print(f'Extract small grib to {small_local_grib_name}')
-            args = [WGRIB_BIN, local_grib_name,
-                    '-small_grib', '-72:-70', '41:42', small_local_grib_name]
-            subprocess.run(args)
-            print(f'Removing {local_grib_name} ...')
-            os.unlink(local_grib_name)
-            return small_local_grib_name
+            return local_grib_name
 
     except ClientError as ex:
         if ex.response['Error']['Code'] == 'NoSuchKey':
@@ -81,6 +84,20 @@ def download_grib_slice(s3, grib_name, work_dir, height_filter, type_filter):
             return None
 
     return None
+
+
+def extract_grib_region(grib_name, region):
+    t = grib_name.split('.')
+    small_local_grib_name = '.'.join(t[0:-1]) + '.small.' + t[-1]
+
+    print(f'Extract small grib to {small_local_grib_name}')
+    lon_bounds = f'{region["nw"]["lon"]}:{region["se"]["lon"]}'
+    lat_bounds = f'{region["se"]["lat"]}:{region["nw"]["lat"]}'
+    cmd_args = [WGRIB_BIN, grib_name,
+                '-small_grib', lon_bounds, lat_bounds, small_local_grib_name]
+    subprocess.run(cmd_args)
+
+    return small_local_grib_name
 
 
 def get_most_recent_grib(work_dir, height_filter, type_filter):
@@ -169,37 +186,67 @@ def get_most_recent_grib(work_dir, height_filter, type_filter):
             grib_name = grib_dir + '/' + f'hrrr.t{f48_run_hour:02d}z.wrfsfcf{i:02d}.grib2'
             grib_list.append(grib_name)
 
-    out_grib_name = (work_dir + os.sep +
-                     f'hrrr-{run_utc.year:04d}-{run_utc.month:02d}-{run_utc.day:02d}-{f18_run_hour:02d}.grib2')
+    out_gribs = []
+    for region in REGIONS:
+        region_name = region['name']
+        out_grib_name = (work_dir + os.sep +
+                         f'hrrr-{region_name}-{run_utc.year:04d}-{run_utc.month:02d}-{run_utc.day:02d}-{f18_run_hour:02d}.grib2')
+        out_grib = open(out_grib_name, 'wb')
+        out_gribs.append((out_grib_name, out_grib))
+        print(f'Creating {out_grib_name} ...')
 
-    print(f'Creating {out_grib_name} ...')
-    with open(out_grib_name, 'wb') as out_grib:
-        for idx, grib in enumerate(grib_list):
-            print(f'Download {idx + 1} {grib}')
-            small_grib = download_grib_slice(s3, grib, work_dir, height_filter, type_filter)
-            if small_grib is not None:
-                in_grib = open(small_grib, 'rb')
+    # Iterate over time
+    for idx, grib in enumerate(grib_list):
+        print(f'Download {idx + 1} {grib}')
+        grib_time_slice = download_grib_time_slice(s3, grib, work_dir, height_filter, type_filter)
+
+        if grib_time_slice is not None:
+            # For each time slice extract the region
+            for reg_idx, region in enumerate(REGIONS):
+                region_grib_name = extract_grib_region(grib_time_slice, region)
+
+                in_grib = open(region_grib_name, 'rb')
+                out_grib = out_gribs[reg_idx][1]
                 shutil.copyfileobj(in_grib, out_grib)
                 in_grib.close()
-                print(f'Removing {small_grib} ...')
-                os.unlink(small_grib)
 
-        print(f'{out_grib_name} created.')
-        return out_grib_name
+                print(f'Removing {region_grib_name} ...')
+                os.unlink(region_grib_name)
+
+            print(f'Removing {grib_time_slice} ...')
+            os.unlink(grib_time_slice)
+
+    for reg_idx, region in enumerate(REGIONS):
+        out_gribs[reg_idx][1].close()
+        print(f'{out_gribs[reg_idx][0]} created.')
+
+    return out_gribs
 
 
 def download_hrrr():
     work_dir = '/tmp'
     height_filter = ['10 m above ground']
     type_filter = ['UGRD', 'VGRD']
-    grib_name = get_most_recent_grib(work_dir, height_filter, type_filter)
+    out_gribs = get_most_recent_grib(work_dir, height_filter, type_filter)
 
-    # Upload the GRIB file to the output bucket
-    s3 = boto3.client('s3')
-    with open(grib_name, 'rb') as f:
-        # Uploading the GRIB file to the output bucket
-        print(f'Uploading {grib_name} to s3://{OUTPUT_GRIB_BUCKET_NAME}/{os.path.basename(grib_name)} ...')
-        s3.upload_fileobj(f, OUTPUT_GRIB_BUCKET_NAME, os.path.basename(grib_name))
+    # Upload the GRIB files to the output bucket
+    grib_list_name = work_dir + os.sep + 'grib-list.txt'
+
+    with open(grib_list_name, 'wt') as grib_list:
+        s3 = boto3.client('s3')
+        for reg_idx, region in enumerate(REGIONS):
+            out_gribs[reg_idx][1].close()
+            grib_name = out_gribs[reg_idx][0]
+            with open(grib_name, 'rb') as f:
+                # Uploading the GRIB file to the output bucket
+                print(f'Uploading {grib_name} to s3://{OUTPUT_GRIB_BUCKET_NAME}/{os.path.basename(grib_name)} ...')
+                s3.upload_fileobj(f, OUTPUT_GRIB_BUCKET_NAME, os.path.basename(grib_name))
+                grib_list.write(f'{os.path.basename(grib_name)}\n')
+
+    grib_list.close()
+    with open(grib_list_name, 'rb') as f:
+        print(f'Uploading {grib_list_name} to s3://{OUTPUT_GRIB_BUCKET_NAME}/{os.path.basename(grib_list_name)} ...')
+        s3.upload_fileobj(f, OUTPUT_GRIB_BUCKET_NAME, os.path.basename(grib_list_name))
 
 
 def handler(event, context):
